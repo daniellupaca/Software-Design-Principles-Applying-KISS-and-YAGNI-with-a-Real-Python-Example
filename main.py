1647aceda6d33e5b0295f25f5602cb10bfb32e86
daniellupaca <103132465+daniellupaca@users.noreply.github.com>
2025-05-01 13:46:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ service = OrderService(repo)
 
 # Create valid order
 order = service.create_order(1, "Client A", 150.0)
-print(f"Created order: {order.customer}, total: {order.total}, status: {order.status}")
-
+print(f"Created order: {order.customer}, total: {order.total}, "
+      f"status: {order.status}")
 # Approve the order
 approved = service.approve_order(1)
 print(f"Updated order: {approved.customer}, status: {approved.status}")
